f5cdec724c4be5dadb53aea7c18d74b68dac894c
Martin Hejl <martinhejl@yahoo.com>
2025-12-03 23:23:18 +0100

    Vytvořený server na portu 3000 s voláním na Google

---
+++ src/search.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const searchBox = document.getElementById('searchBox');
+    const searchButton = document.getElementById('searchButton');
+    const resultsContainer = document.getElementById('results');
+
+    
+})
